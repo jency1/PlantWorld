28d895b72b107fd36c2c11c2d3760268f6e09678
Shreya-1305 <paintershreya@gmail.com>
2024-10-02 00:15:42 +0530

    images changed

--- src/HomePage/FollowOnInstagram.jsx
+++ src/HomePage/FollowOnInstagram.jsx
@@ -1,60 +1,66 @@
+import styles from "./FollowOnInstagram.module.css";
+
 function FollowOnInstagram() {
   return (
     <div className="d-flex flex-column align-items-center ">
       <div className="h1 mt-5 text-success text-center">
         Follow us on Instagram
       </div>
-      <div
-        className="container mt-2 mb-5 "
-        style={{ padding: "0 9rem 0 9rem" }}
-      >
+      <div className={`${styles["main-container"]} container mt-2 mb-5`}>
         <div className="row">
-          <div className="col-8 container p-8">
-            <div className="row ">
-              <div className="col-5 text-center mt-3">
+          <div className="col-12 col-md-8 container p-8">
+            <div className="row">
+              <div className="col-6 text-center mt-3">
                 <img
-                  src="/public/Shop By Category/image-1.png"
+                  src="/public/Shop By Category/image-1.jpg"
                   alt=""
                   className="w-100 h-100 img-fluid rounded"
                 />
               </div>
-              <div className="col-7 text-center mt-3">
+              <div className="col-6 text-center mt-3">
                 <img
-                  src="/public/Shop By Category/image-2.png"
+                  src="/public/Shop By Category/image-2.jpg"
                   alt=""
                   className="w-100  h-100 img-fluid rounded"
                 />
               </div>
             </div>
             <div className="row">
-              <div className="col-7 text-center mt-3">
+              <div className="col-8 text-center mt-3">
                 <img
-                  src="/public/Shop By Category/image-3.png"
+                  src="/public/Shop By Category/image-3.jpg"
                   alt=""
                   className="w-100  h-100 img-fluid rounded"
                 />
               </div>
-              <div className="col-5 text-center mt-3">
+              <div className="col-4 text-center mt-3">
                 <img
-                  src="/public/Shop By Category/image-4.png"
+                  src="/public/Shop By Category/image-4.jpg"
                   alt=""
                   className="w-100 h-100 img-fluid rounded"
                 />
               </div>
             </div>
             <div className="row">
-              <div className="col-12 text-center mt-3">
+              <div className="col-6 text-center mt-3">
                 <img
-                  src="/public/Shop By Category/image-5.png"
+                  src="/public/Shop By Category/image-5.jpg"
+                  alt=""
+                  className="w-100 h-100 img-fluid rounded"
+                />
+              </div>
+              <div className="col-6 text-center mt-3">
+                <img
+                  src="/public/Shop By Category/image-6.jpg"
                   alt=""
                   className="w-100 h-100 img-fluid rounded"
                 />
               </div>
             </div>
           </div>
-          <div className="col-4 text-center mt-2 ">
+          <div className={`${styles.longImage} col-4 text-center mt-2`}>
             <img
-              src="/public/Shop By Category/long-image.png"
+              src="/public/Shop By Category/long-image.jpg"
               alt=""
               className="h-100 w-100 img-fluid rounded "
             />
