0f127b9006b3d48a8858bb7b649494854c79ec85
jency <jencymaheshwari2004@gmail.com>
2024-10-21 21:54:53 +0530

    home page updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ function App() {
   return (
     <>
       {/* <Navbar /> */}
-      {/* <HomePage /> */}
+      <HomePage />
       {/* <ShopPageLayout /> */}
       {/* <DescriptionLayout /> */}
-      <LoginPage />
+      {/* <LoginPage /> */}
       {/* <LP /> */}
     </>
   );
