994a37a62e59194c2dc276d7660d71f8359a552e
Shreya-1305 <paintershreya@gmail.com>
2025-01-16 14:10:26 +0530

    basic model created

--- Backend/model/plantModel.js
+++ Backend/model/plantModel.js
@@ -6,13 +6,52 @@ const plantSchema = new mongoose.Schema({
     required: [true, 'A plant must have a name'],
     unique: true,
   },
-  rating: {
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A plant must have a price'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A plant must have a description'],
+  },
+  careInstructions: {
+    type: String,
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: [true, 'A plant must have a category'],
+  },
+  tag: {
+    type: String,
+    required: [true, 'A plant must have a tag'],
+  },
+  color: {
+    type: [String],
+    required: [true, 'A plant must have at least one color'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A plant must have a cover image'],
+  },
+  plantCareTips: {
+    type: [String], // Array of strings to store multiple tips
+    required: [true, 'A plant must have care tips'],
+    validate: {
+      validator: function (val) {
+        return val.length > 0; // Ensure at least one care tip is provided
+      },
+      message: 'There should be at least one care tip provided',
+    },
   },
 });
 
