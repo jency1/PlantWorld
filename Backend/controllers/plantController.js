be1e8d8fd59f0f7628c85e7e1a98d14df4f246d7
Shreya-1305 <paintershreya@gmail.com>
2025-01-18 19:22:58 +0530

    limting fields added for the get all plants

--- Backend/controllers/plantController.js
+++ Backend/controllers/plantController.js
@@ -36,6 +36,15 @@ exports.getAllPlants = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 3) Field limiting
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // EXECUTE QUERY
 
     const plants = await query;
