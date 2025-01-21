3b646daa77f01dd75a37bab5674042fe1204cdc9
Shreya-1305 <paintershreya@gmail.com>
2025-01-21 10:59:33 +0530

    error controller created

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const morgan = require('morgan');
 const plantRouter = require('./routes/plantRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -13,11 +15,10 @@ app.use(express.json());
 app.use('/api/plants', plantRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
+app.use(globalErrorHandler);
 
 // const userRouter = express.Router();
 // app.use('/api/user', userRouter);
